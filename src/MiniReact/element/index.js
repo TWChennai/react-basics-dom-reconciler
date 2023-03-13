92a73fcff4716237b79f7c2a9c154bc567356c57
Balaji Vijayakumar <kuttibalaji.v6@gmail.com>
2023-03-13 19:11:34 +0530

    fix: eslint issues
    Signed-off-by: Balaji Vijayakumar <kuttibalaji.v6@gmail.com>

--- src/MiniReact/element/index.js
+++ src/MiniReact/element/index.js
@@ -4,6 +4,7 @@
  * @param  {...any} args - the children array
  * @returns {object} - to be called by tevreact.render
  */
+// eslint-disable-next-line no-unused-vars
 export function createElement(type, configObject, ...args) {
   // TODO: to be implemented
   return { type };
